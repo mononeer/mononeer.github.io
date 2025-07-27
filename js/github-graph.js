797a5992a3a1a3f33cc5f05ae786aebd340199ea
Mononeer <cacophonn@gmail.com>
2025-07-27 18:08:22 +0200

    Update github-graph.js

--- js/github-graph.js
+++ js/github-graph.js
@@ -1,1 +1,9 @@
-
+fetch('https://api.github.com/users/mononeer')
+  .then(r => r.json())
+  .then(u => {
+    const img = document.createElement('img');
+    img.src = `https://ghchart.rshah.org/00BFFF/mononeer`;
+    img.alt = "GitHub contributions";
+    img.style.maxWidth = '100%';
+    document.getElementById('gh-graph').appendChild(img);
+  });
